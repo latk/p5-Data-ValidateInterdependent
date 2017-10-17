5dd6b6875e204db3118b8c368b5c8b8e3f005744
Lukas Atkinson <amon@cpan.org>
2017-10-17 22:35:27 +0200

    write documentation

--- lib/Data/ValidateInterdependent.pm
+++ lib/Data/ValidateInterdependent.pm
@@ -15,17 +15,85 @@ Data::ValidateInterdependent - safely validate interdependent parameters
 
 =head1 SYNOPSIS
 
-TODO
+    use Data::ValidateInterdependent;
+
+    state $validator =
+        Data::ValidateInterdependent->new
+        # inject a constant value
+        ->const(generator => 'perl')
+        # take an input parameter without validation
+        ->param('description')
+        # create variables "x", "y", "z" from parameter "coords"
+        ->validate(['x', 'y', 'z'], '$coords', sub {
+            my ($coords) = @_;
+            die "Coords must contain 3 elements" unless @$coords == 3;
+            my ($x, $y, $z) = @$coords;
+            return { x => $x, y => $y, z => $z };
+        })
+        # create variable "title" from parameter "title"
+        # and from validated variables "x", "y", "z".
+        ->validate('title', ['$title, 'x', 'y', 'z'], sub {
+            my ($title, $x, $y, $z) = @_;
+            $title //= "Object at ($x, $y, $z)";
+            return { title => $title };
+        });
+
+    my $variables = $validator->run(%config);
 
 =head1 DESCRIPTION
 
-TODO
+The problem: you need to validate some configuration.
+But validation of one field depends on other fields,
+or default values are taken from other parts of the config.
+These dependencies can be daunting.
 
-Uses static single assignment (SSA) to verify consistency.
+This module makes the dependencies between different validation steps
+more explicit:
+Each step declares which variables it provides,
+and which variables or input parameters it consumes.
+The idea of
+L<Static Single Assignment|https://en.wikipedia.org/wiki/Static_single_assignment_form>
+allows us to check basic consistency properties when the validator is assembled:
 
-A Rule is a List[Variable] => List[Variable] transformation.
+=over
+
+=item *
+
+The validator will provide all declared input variables.
+Because there is no branching,
+it is not possible to forget a variable.
+
+=item *
+
+All variables are declared before they are used.
+It is not possible to accidentally read an unvalidated value.
+
+=item *
+
+Each variable is only initialized once.
+It is not possible to accidentally overwrite a variable.
+
+=back
+
+=head2 Terminology
+
+A B<parameter> is an unvalidated input value.
+A parameter called C<name> can be addressed with the symbol C<$name>,
+i.e. with a prepended C<$> character.
+If no such parameter exists, its value will be C<undef>.
+
+A B<variable> is a validated field that will be written exactly once.
+A variable called C<name> is addressed with the symbol C<name>,
+i.e. without any changes.
+
+A B<validation rule> is a callback that initializes one or more variables.
+It receives a list with any number of parameters and variables.
 
 =head1 METHODS
+
+Unless explicitly noted,
+all methods return the object itself
+so that you can chain methods.
 
 =cut
 
@@ -83,12 +151,29 @@ sub _declare_param {
 
 =head2 const
 
-TODO
+    $validator = $validator->const(name => $value, ...);
+
+Declare one or more variables with a constant value.
+
+In most cases this is not necessary
+because you could use Perl variables
+to make data accessible to all pipeline steps.
+
+Note that this method cannot provide default values for a variable,
+since all variables are write-once.
+
+This method is functionally equivalent to:
+
+    $validator->validate(['name', ...], [], sub {
+        return { name => $value, ... };
+    });
 
 =cut
 
 sub const {
     my ($self, %values) = @_;
+    # TODO must not be empty
+
     _declare_variable($self, sort keys %values);
     push @{ $self->_rules }, [const => \%values];
     return $self;
@@ -96,12 +181,33 @@ sub const {
 
 =head2 param
 
-TODO
+    $validator = $validator->param('name', { variable => 'parameter' }, ...);
+
+Declare variables that take their value directly from input parameters
+without any validation.
+
+The arguments may be variable names,
+in which case the value is taken from the parameter of the same name.
+The arguments may also be a hash ref,
+which maps variable names to parameters.
+These names are not symbols,
+so you must not include the C<$> for parameter symbols.
+
+Absolutely no validation will be performed.
+If the parameter does not exist, the variable will be C<undef>.
+
+This method is functionally equivalent to:
+
+    $validator->validate(['name', 'variable', ...], ['$name', '$parameter'], sub {
+        my ($name, $parameter, ...) = @_;
+        return { name => $name, variable => $parameter, ... };
+    });
 
 =cut
 
 sub param {
     my ($self, @items) = @_;
+    # TODO must not be empty
 
     my %mapping;
     for my $item (@items) {
@@ -119,13 +225,53 @@ sub param {
 
 =head2 validate
 
-TODO
+    $validator = $validator->validate($output, $input, sub { ... });
+
+Perform a validation step.
+
+B<$output> declares the variables which are assigned by this validation step.
+It may either be a single variable name,
+or an array ref with one or more variable names.
+
+B<$input> declares dependencies on other variables or input parameters.
+It may either be a single symbol,
+or an array ref of symbols.
+The array ref may be empty.
+A symbol can be the name of a variable,
+or a C<$> followed by the name of a parameter.
+
+B<sub { ... }> is a callback that peforms the validation step.
+The callback will be invoked with the values of all I<$input> symbols,
+in the order in which they were listed.
+Note that a parameter will have C<undef> value if it doesn't exist.
+The callback must return a hash ref that contains all variables to be assigned.
+The hash keys must match the declared I<$output> variables exactly.
+
+The returned hash ref will be modified.
+If other code depends on this hash ref, return a copy instead.
+
+B<Throws> when an existing variable was re-declared.
+All variables are write-once.
+You cannot reassign them.
+
+B<Throws> when an undeclared variable was used.
+You must declare all variables before you can use them.
+
+B<Example:> Reading multiple inputs:
+
+    # "x" and "y" are previously declared variables.
+    # "foo" is an input parameter.
+    $validator->validate('result', ['x', '$foo', 'y'], sub {
+        my ($x, $foo, $y) = @_;
+        $foo //= $y;
+        return { result => $x + $foo };
+    });
 
 =cut
 
 sub validate {
     my ($self, $output, $input, $callback) = @_;
-    $output = [_parse_params($output)];
+    $output = [_parse_params($output)];  # TODO must not be empty
     $input = [_parse_params($input)];
 
     my @vars;
@@ -155,13 +301,24 @@ sub validate {
 
 =head2 run
 
-    my $validated = $v->run(%params);
+    my $variables = $validator->run(%params);
 
-TODO
+Run the validator with a given set of params.
+A validator instance can be run multiple times.
 
-B<Returns:> a hashref with all variables.
+B<%params> is a hash of all input parameters.
+The hash may be empty.
 
-B<Throws:> when unknown parameters were provided.
+B<Returns:> a hashref with all output variables.
+You may want to remove variables from this hashref
+if some validation rules needed helper variables.
+
+B<Throws:> when unknown parameters were provided
+(but see L<ignore_unknown()|/ignore_unknown>).
+
+B<Throws:> when a rule callback did not return a suitable value:
+either it was not a hash ref,
+or the hash ref did not assign exactly the $output variables.
 
 =cut
 
@@ -229,9 +386,13 @@ sub run {
 
 =head2 ignore_unknown
 
-    $v->ignore_unknown;
+    $validator = $validator->ignore_unknown;
 
-TODO
+Ignore unknown parameters.
+If this flag is not set, the L<run()|/run> method will die
+when unknown parameters were provided.
+A parameter is unknown when no validation rule or param assignment
+reads from that parameter.
 
 =cut
 
@@ -243,7 +404,7 @@ sub ignore_unknown {
 
 =head2 provided
 
-    my @names = $v->provided;
+    my @names = $validator->provided;
 
 TODO
 
