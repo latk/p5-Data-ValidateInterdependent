bff47506addd4c450460c37865794e8275533407
Lukas Atkinson <amon@cpan.org>
2017-10-17 18:28:27 +0200

    basic functionality

--- lib/Data/ValidateInterdependent.pm
+++ lib/Data/ValidateInterdependent.pm
@@ -40,10 +40,46 @@ has _rules => (
     default => sub { [] },
 );
 
-has _required_parameters => (
+has _expected_params => (
     is => 'ro',
     default => sub { {} },
 );
+
+has _ignore_unknown => (
+    is => 'rw',
+    default => 0,
+);
+
+sub _parse_params {
+    my ($spec) = @_;
+    return @$spec if ref $spec eq 'ARRAY';
+    return $spec;
+}
+
+sub _declare_variable {
+    my ($self, @names) = @_;
+
+    my $known = $self->_variables;
+    for my $var (@names) {
+        if ($known->{$var}) {
+            croak qq(Variable cannot be declared twice: $var);
+        }
+        else {
+            $known->{$var} = 1;
+        }
+    }
+
+    return;
+}
+
+sub _declare_param {
+    my ($self, @names) = @_;
+    my $expected_params = $self->_expected_params;
+
+    $expected_params->{$_} = 1 for @names;
+
+    return;
+}
 
 =head2 const
 
@@ -53,7 +89,9 @@ TODO
 
 sub const {
     my ($self, %values) = @_;
-    ...
+    _declare_variable($self, sort keys %values);
+    push @{ $self->_rules }, [const => \%values];
+    return $self;
 }
 
 =head2 param
@@ -63,8 +101,20 @@ TODO
 =cut
 
 sub param {
-    my ($self, @params) = @_;
-    ...
+    my ($self, @items) = @_;
+
+    my %mapping;
+    for my $item (@items) {
+        $item = { $item => $item } if ref $item ne 'HASH';
+        @mapping{ keys %$item } = values %$item;
+    }
+
+    _declare_param($self, sort values %mapping);
+    _declare_variable($self, sort keys %mapping);
+
+    push @{ $self->_rules }, [param => \%mapping];
+
+    return $self;
 }
 
 =head2 validate
@@ -75,19 +125,129 @@ TODO
 
 sub validate {
     my ($self, $output, $input, $callback) = @_;
-    ...
+    $output = [_parse_params($output)];
+    $input = [_parse_params($input)];
+
+    my @vars;
+    my @args;
+    for (@$input) {
+        if (/^\$/) {
+            push @args, s/^\$//r;
+        }
+        else {
+            push @vars, $_;
+        }
+    }
+
+    _declare_param($self, @args) if @args;
+
+    my $known_variables = $self->_variables;
+
+    if (my @unknown = grep { not $known_variables->{$_} } @vars) {
+        croak qq(Validation rule "@$output" depends on undeclared variables: ), join q(, ) => sort @unknown;
+    }
+
+    _declare_variable($self, @$output);
+
+    push @{ $self->_rules }, [rule => $output, $input, $callback];
+    return $self;
 }
 
 =head2 run
 
+    my $validated = $v->run(%params);
+
 TODO
+
+B<Returns:> a hashref with all variables.
+
+B<Throws:> when unknown parameters were provided.
 
 =cut
 
 sub run {
     my ($self, %params) = @_;
-    ...
+
+    unless ($self->_ignore_unknown) {
+        my $expected_params = $self->_expected_params;
+        if (my @unknown = grep { not $expected_params->{$_} } keys %params) {
+            croak qq(Unknown parameters: ), join q(, ) => sort @unknown;
+        }
+    }
+
+    my %variables;
+
+    my $get_arg = sub {
+        my ($name) = @_;
+        return $params{$name} if $name =~ s/^\$//;
+        return $variables{$name};
+    };
+
+    RULE:
+    for my $rule (@{ $self->_rules }) {
+        my ($type, @rule_args) = @$rule;
+
+        if ($type eq 'const') {
+            my ($values) = @rule_args;
+            @variables{keys %$values} = values %$values;
+            next RULE;
+        }
+
+        if ($type eq 'param') {
+            my ($mapping) = @rule_args;
+            @variables{ keys %$mapping } = @params{ values %$mapping };
+            next RULE;
+        }
+
+        if ($type eq 'rule') {
+            my ($provided, $required, $callback) = @rule_args;
+
+            my $result = $callback->(map { $get_arg->($_) } @$required);
+
+            for my $var (@$provided) {
+                if (exists $result->{$var}) {
+                    $variables{$var} = delete $result->{$var};
+                }
+                else {
+                    croak qq(Validation rule "@$provided" must return parameter $var);
+                }
+            }
+
+            if (my @unknown = keys %$result) {
+                croak qq(Validation rule "@$provided" returned unknown variables: ),
+                    join q(, ) => sort @unknown;
+            }
+
+            next RULE;
+        }
+
+        die "Unknown rule type: $type";
+    }
+
+    return \%variables;
 }
+
+=head2 ignore_unknown
+
+    $v->ignore_unknown;
+
+TODO
+
+=cut
+
+sub ignore_unknown {
+    my ($self) = @_;
+    $self->_ignore_unknown(1);
+    return $self;
+}
+
+=head2 provided
+
+    my @names = $v->provided;
+
+TODO
+
+=cut
 
 =head1 SUPPORT
 
--- t/basic.t
+++ t/basic.t
@@ -4,7 +4,130 @@ use strict;
 use warnings;
 
 use Test::More;
+use Test::Exception;
 
 use_ok 'Data::ValidateInterdependent';
+
+sub new { Data::ValidateInterdependent->new(@_) }
+
+subtest q(it can be empty) => sub {
+    my $valid = new->run();
+    is_deeply $valid, {};
+};
+
+subtest q(it rejects unknown params) => sub {
+    throws_ok { new->run(foo => 42, bar => 42) }
+        qr/^Unknown parameters: bar, foo at /;
+};
+
+subtest q(it does not die when unknowns are ignored) => sub {
+    my $valid = new->ignore_unknown->run(x => 1, y => 2, z => 3);
+    is_deeply $valid, {};
+};
+
+subtest q(it can add constant values) => sub {
+    my $valid = new->const(x => 'a', b => 3)->run();
+    is_deeply $valid, { b => 3, x => 'a' };
+};
+
+subtest q(rule can provide calculated values) => sub {
+    my $valid = new
+        ->validate('x', [], sub {
+            return { x => 17 },
+        })
+        ->run();
+    is_deeply $valid, { x => 17 };
+};
+
+subtest q(rule can return multiple variables) => sub {
+    my $valid = new
+        ->validate(['x', 'y'], [], sub {
+            return { x => 3, y => 5 };
+        })
+        ->run();
+    is_deeply $valid, { x => 3, y => 5 },
+};
+
+subtest q(rule can only provide declared variables) => sub {
+    my $v = new
+        ->validate(['y', 'x'], [], sub {
+            return { x => 17, y => 3, a => 9, b => 42 };
+        });
+    throws_ok { $v->run() }
+        qr/^Validation rule "y x" returned unknown variables: a, b at /;
+};
+
+subtest q(rule must provide all declared variables) => sub {
+    my $v = new
+        ->validate(['y', 'x'], [], sub {
+            return { x => 3 };
+        });
+
+    throws_ok { $v->run() }
+        qr/Validation rule "y x" must return parameter y at /;
+};
+
+subtest q(rule can use parameters) => sub {
+    my $valid = new
+        ->validate('y', '$x', sub {
+            my ($x) = @_;
+            return { y => $x + 3 };
+        })
+        ->run(x => 10);
+    is_deeply $valid, { y => 13 };
+};
+
+subtest q(rule can use previous variables) => sub {
+    my $valid = new
+        ->validate('x', [], sub {
+            return { x => 10 };
+        })
+        ->validate('y', 'x', sub {
+            my ($x) = @_;
+            return { y => $x + 3 };
+        })
+        ->run;
+    is_deeply $valid, { x => 10, y => 13 };
+};
+
+subtest q(rule dependencies must have been declared) => sub {
+    throws_ok { new->validate(['y', 'x'], ['b', 'a'], sub { ... }) }
+        qr/Validation rule "y x" depends on undeclared variables: a, b at /;
+};
+
+subtest q(rule dependency cannot be declared twice) => sub {
+    my $v = new->validate('x', [], sub { ... });
+    throws_ok { $v->validate('x', [], sub { ... }) }
+        qr/Variable cannot be declared twice: x at /;
+};
+
+subtest q(const values also count as declared variable) => sub {
+    my $valid = new
+        ->const(foo => 3)
+        ->validate('bar', 'foo', sub {
+            my ($foo) = @_;
+            return { bar => $foo + 10 };
+        })
+        ->run;
+    is_deeply $valid, { foo => 3, bar => 13 };
+};
+
+subtest q(it can consume params) => sub {
+    my $valid = new
+        ->param('a', { b => 'x', c => 'y' })
+        ->run(a => 1, x => 3, y => 7);
+    is_deeply $valid, { a => 1, b => 3, c => 7 };
+};
+
+subtest q(params also count as declared variable) => sub {
+    my $valid = new
+        ->param('x')
+        ->validate('a', 'x', sub {
+            my ($x) = @_;
+            return { a => $x * 4 };
+        })
+        ->run(x => 2);
+    is_deeply $valid, { x => 2, a => 8 };
+};
 
 done_testing;
